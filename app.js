e4840cdfa4961c5df64e8b9a545fd77c8558efa8
Diwakar433 <81136470+Diwakar433@users.noreply.github.com>
2022-08-23 09:53:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const form = document.querySelector('#searchForm');
+const res = document.querySelector('#tableResult');
+form.addEventListener('submit',(e)=>{
+
+    e.preventDefault();
+
+    const ctype = form.elements.coinType.value;
+      
+    
+    // console.log(ctype);
+    fetchPrice(ctype);
+
+});
+
+const fetchPrice = async(ctype)=>{
+    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=INR`);
+
+    // console.log(r.data.coin.price);
+    const price = r.data.coin.price;
+    const volume = r.data.coin.volume;
+    const change = r.data.coin.priceChange1d;
+    const base = r.data.coin.name;
+    const target = "INR";
+    // const time = r.data.coin.time;
+
+    res.innerHTML = `<tr style="background-color:blue; color:white; font-weight:700 ">
+    <td>
+        property
+    </td>
+    <td>
+        value
+    </td>
+</tr>
+<tr>
+    <td>
+        ${base}
+    </td>
+    <td>
+        ${price} ${target}
+    </td>
+</tr>
+
+<tr>
+    <td>
+        Volume
+    </td>
+    <td>
+        ${volume}
+    </td>
+</tr>
+
+<tr>
+    <td>
+        Change
+    </td>
+        
+    <td>
+        ${change}
+    </td>
+</tr>
+
+`;
+    
+}
